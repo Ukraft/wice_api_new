04e7dbbccd868f770744310f1c7d7c74e3765fdf
uwe <uwe@uwe.m2>
2018-03-21 12:41:51 +0100

    test 5

--- documentation/schemas.js
+++ documentation/schemas.js
@@ -6,7 +6,7 @@ export default fromJS([
     // The "getting started" schema provides table-of-contents links
     // for the sections in src/client/introduction.js.  You are free
     // to customize or remove both the schema and the introduction component.
-    require('./getting-started.json'),
+    // require('./getting-started.json'),
     require('../schemas/person.json'),
     require('../schemas/organization.json'),
     require('../schemas/article.json')
